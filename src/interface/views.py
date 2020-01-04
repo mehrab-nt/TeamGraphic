254e9f32622b089c145a066096d7ba9a9f944272
Mehrab_NT <mehrab_nt@yahoo.com>
2020-01-04 13:19:32 +0330

    Url Start :)

--- src/interface/views.py
+++ src/interface/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, get_list_or_404, redirect, reverse
 
-# Create your views here.
+
+def home_view(request):
+    context = {
+
+    }
+    return render(request, 'main/home.html', context)
+    # if request.POST:
+    #     try:
+    #         token = request.POST.get('csrfmiddlewaretoken')
+
