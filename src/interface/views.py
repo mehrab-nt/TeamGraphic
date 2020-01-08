8f546531249d6fab5e587876692908cbb28a257c
Mehrab_NT <mehrab_nt@yahoo.com>
2020-01-08 10:35:24 +0330

    Interface !!! view.py

--- src/interface/views.py
+++ src/interface/views.py
@@ -2,12 +2,12 @@ from django.shortcuts import render
 from .models import SlideShow
 
 
-# def home_view(request):
-#     context = {
-#         'user': request.user,
-#         'slideshow_list': SlideShow.objects.filter(active=True)
-#     }
-#     return render(request, 'main/home.html', context)
-    # if request.POST:
-    #     try:
-    #         token = request.POST.get('csrfmiddlewaretoken')
+def home_view(request):
+    context = {
+        'user': request.user,
+        'slideshow_list': SlideShow.objects.filter(active=True)
+    }
+    return render(request, 'main/home.html', context)
+# if request.POST:
+#     try:
+#         token = request.POST.get('csrfmiddlewaretoken')
