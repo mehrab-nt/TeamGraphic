e5ba37dd78e840244b49e0819f76c3cea70790bd
mehrab <mehrab_nt@yahoo.com>
2025-07-03 03:20:22 +0330

    Change Api Urls

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,18 @@
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+from rest_framework.routers import DefaultRouter
+from user.views import UserViewSet, RoleViewSet, IntroductionViewSet
+from employee.views import EmployeeViewSet
 
+
+router = DefaultRouter()
+router.register(r'user', UserViewSet, basename='user')
+router.register(r'role', RoleViewSet, basename='role')
+router.register(r'introduction', IntroductionViewSet, basename='introduction')
+router.register(r'employee', EmployeeViewSet, basename='employee')
 urlpatterns = [
     path('schema/', SpectacularAPIView.as_view(), name='schema'),
     path('schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
-    path('', include('user.urls')),
+    path('', include(router.urls)),
 ]
--- user/urls.py
+++ user/urls.py
@@ -1,14 +1,8 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import UserViewSet, RoleViewSet, IntroductionViewSet, CustomTokenRefreshView, CustomTokenVerifyView
+from django.urls import path
+from .views import CustomTokenRefreshView, CustomTokenVerifyView
 
-router = DefaultRouter()
-router.register(r'users', UserViewSet, basename='users')
-router.register(r'roles', RoleViewSet, basename='roles')
-router.register(r'introductions', IntroductionViewSet, basename='introductions')
 
 urlpatterns = [
     path('token/refresh/', CustomTokenRefreshView.as_view(), name='token_refresh'),
     path('token/verify/', CustomTokenVerifyView.as_view(), name='token_verify'),
-    path('', include(router.urls)),
 ]
