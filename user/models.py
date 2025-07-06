70c04e1bf231838ae4617538a3834336298d5da5
mehrab <mehrab_nt@yahoo.com>
2025-07-06 15:13:06 +0330

    User Signal Improved

--- user/models.py
+++ user/models.py
@@ -55,13 +55,11 @@ class User(AbstractUser):
                 return prefix
 
     def save(self, *args, **kwargs):
-        try:
+        is_update = self.pk is not None and User.objects.filter(pk=self.pk).exists()
+        if is_update:
             old_user = User.objects.get(pk=self.pk)
-            if User.objects.get(pk=self.pk): # Means Update
-                if self.phone_number != old_user.phone_number:
-                    self.phone_number_verified = False
-        except ObjectDoesNotExist:
-            pass
+            if self.phone_number != old_user.phone_number:
+                self.phone_number_verified = False
         if not self.public_key or 'tg-' not in self.public_key:
             self.public_key = self.generate_unique_key(User,'public_key', 8, 'tg-')
         if not self.private_key or len(self.private_key) != 16:
--- user/signals.py
+++ user/signals.py
@@ -1,21 +1,25 @@
-from logging import NullHandler
-
-from django.db.models.signals import post_delete, post_save
+from django.db.models.signals import post_delete, post_save, pre_save
 from django.dispatch import receiver
+from django.db.models import F
 from .models import Role, User, UserProfile
 
 
-# MEH: Lessen if default role remove! why?
 @receiver(post_delete, sender=Role)
-def assign_default_role_to_users(sender, instance, **kwargs):
+def role_post_delete(sender, instance, **kwargs):
+    """
+    MEH: Lessen if default role remove! why?
+    """
     default_role = Role.objects.filter(is_default=True).first()
     if default_role:
         User.objects.filter(role__isnull=True, is_employee=False).update(role=default_role)
 
 
-# MEH: Create profile for user automatically when a new user created ...
 @receiver(post_save, sender=User)
-def create_profile_for_new_user(sender, instance, created, **kwargs):
+def user_post_save(sender, instance, created, **kwargs):
+    """
+    MEH: Clean User after save.
+    like -> Create profile for user automatically when a new user created ...
+    """
     flag = False
     if not instance.user_profile:
         profile = UserProfile.objects.create()
@@ -27,13 +31,42 @@ def create_profile_for_new_user(sender, instance, created, **kwargs):
     if instance.is_employee and instance.role:
         instance.role = None
         flag = True
+    if instance.introduce_from:
+        if created:
+            instance.introduce_from.number = F('number') + 1
+            instance.introduce_from.save(update_fields=['number'])
     if flag:
         instance.save()
 
 
-# MEH: If user deleted in any way, profile also deleted!!!
+@receiver(pre_save, sender=User)
+def user_pre_save(sender, instance, **kwargs):
+    """
+    Clean user data before save!
+    """
+    if instance.pk:
+        try:
+            old_user = User.objects.get(pk=instance.pk)
+        except User.DoesNotExist:
+            return
+        if old_user.introduce_from != instance.introduce_from:
+            if old_user.introduce_from:
+                old_user.introduce_from.number = F('number') + 1
+                old_user.introduce_from.save(update_fields=['number'])
+            if instance.introduce_from:
+                instance.introduce_from.number = F('number') + 1
+                instance.introduce_from.save(update_fields=['number'])
+
+
 @receiver(post_delete, sender=User)
-def delete_profile_after_delete_user(sender, instance, **kwargs):
+def user_post_delete(sender, instance, **kwargs):
+    """
+    MEH: Mines introduce counter by 1 & If User deleted in any way, profile also deleted!!!
+    (Opposite direction Handle in on_delete.CASCADE()
+    """
+    if instance.introduce_from:
+        instance.introduce_from.number = F('number') - 1
+        instance.introduce_from.save(update_fields=['number'])
     try:
         instance.user_profile.delete()
     except UserProfile.DoesNotExist:
