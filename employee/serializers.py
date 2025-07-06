f40fdd87c8ef540a66ddc95d25d274e9d35e15af
mehrab <mehrab_nt@yahoo.com>
2025-07-07 03:02:16 +0330

    Continue Employee Serializer!

--- employee/serializers.py
+++ employee/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
-from .models import Employee
+
+from api.models import ApiItem
+from .models import Employee, EmployeeLevel
 from user.models import User
 from user.serializers import UserSerializer
 from rest_framework.exceptions import PermissionDenied
@@ -7,8 +9,11 @@ from api.mixins import CustomModelSerializer
 from api.responses import *
 
 
-# MEH: Handle important field for Employee
 class UserEmployeeSerializer(UserSerializer):
+    """
+    MEH: Handle important field form User model in Employee Serializer
+    Create & Update Handle in UserSerializer...
+    """
     is_employee = serializers.HiddenField(default=True)
     class Meta:
         model = User
@@ -17,32 +22,30 @@ class UserEmployeeSerializer(UserSerializer):
         read_only_fields = ['id', 'date_joined', 'is_active']
 
 
-# MEH: Main Employee full information
 class EmployeeSerializer(CustomModelSerializer):
-    user = UserEmployeeSerializer(required=False)
+    """
+    MEH: Main Employee full information
+    """
+    user = UserEmployeeSerializer(required=False) # MEH: Nested (User Model) Information
+    level = serializers.StringRelatedField()
 
     class Meta:
         model = Employee
         fields = '__all__'
-        read_only_fields = ('level', 'rate')
+        read_only_fields = ('rate',)
 
-    # MEH: Nested create employee with user information and profile (Just for Admin work) just single Employee
-    def create(self, validated_data, **kwargs):
-        if isinstance(validated_data, list): # MEH: ignore bulk create
-            raise PermissionDenied(TG_PERMISSION_DENIED)
-        else: # MEH: Handle single create
-            user_data = validated_data.pop('user', {})
-            if user_data:
-                user_serializer = UserEmployeeSerializer(data=user_data)
-                user_serializer.is_valid(raise_exception=True)
-                user = user_serializer.save()
-                employee = Employee.objects.create(**validated_data, user=user)
-                return employee
-            else:
-                raise serializers.ValidationError({'user': TG_DATA_WRONG})
+    def create(self, validated_data, **kwargs): # MEH: Nested create Employee with User information and Profile (Just for Admin work) just single Employee
+        user_data = validated_data.pop('user', {})
+        if user_data:
+            user_serializer = UserEmployeeSerializer(data=user_data)
+            user_serializer.is_valid(raise_exception=True)
+            user = user_serializer.save()
+            employee = Employee.objects.create(**validated_data, user=user)
+            return employee
+        else:
+            raise serializers.ValidationError({'employee': TG_DATA_WRONG})
 
-    # MEH: Nested update for employee with profile
-    def update(self, instance, validated_data, **kwargs):
+    def update(self, instance, validated_data, **kwargs): # MEH: Nested update for Employee with User & Profile
         user_data = validated_data.pop('user', {})
         if user_data:
             user_serializer = UserEmployeeSerializer(instance=instance.user, data=user_data, partial=(self.context.get('request').method == 'PATCH'))
@@ -52,3 +55,33 @@ class EmployeeSerializer(CustomModelSerializer):
             setattr(instance, attr, value)
         instance.save()
         return instance
+
+
+class EmployeeLevelSerializer(CustomModelSerializer):
+    """
+    MEH: Main Employee level full information
+    """
+    manager = serializers.StringRelatedField()
+    class Meta:
+        model = EmployeeLevel
+        fields = ['id', 'title', 'description', 'is_active', 'manager']
+
+    def create(self, validated_data, **kwargs):
+        employee_level = EmployeeLevel(**validated_data, **kwargs) # MEH: for parse manager in **kwargs
+        employee_level.save()
+        return employee_level
+
+
+class EmployeeLevelSetSerializer(CustomModelSerializer):
+    """
+    MEH: Handle Post Employee to Level for Set that Level on Employee
+    """
+    full_name = serializers.SerializerMethodField()
+    level_display = serializers.StringRelatedField(source='level')
+    class Meta:
+        model = Employee
+        fields = ['id', 'full_name', 'level', 'level_display']
+
+    @staticmethod
+    def get_full_name(obj):
+        return str(obj)
