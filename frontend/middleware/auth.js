c76ed123240a8184fc024a38af7a5d72e9585f8d
mehrab <mehrab_nt@yahoo.com>
2025-07-15 06:56:31 +0330

    Clear & Remove

--- frontend/middleware/auth.js
+++
@@ -1,8 +0,0 @@
-// middleware/auth.js
-
-export default defineNuxtRouteMiddleware((to, from) => {
-  const token = localStorage.getItem('access_token')
-  if (!token) {
-    return navigateTo('/login')  // Redirect to login page if not authenticated
-  }
-})
